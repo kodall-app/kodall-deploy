5fb3d119955c75eee73b86d33ab8232bfdfeb765
iulian.radulescu <iulian.radulescu@onesoftware.ro>
2026-08-20 16:18:48 +0300

    tests config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/cli.ts", "src/core/types.ts", "**/*.d.ts"],
     },
   },
 });
